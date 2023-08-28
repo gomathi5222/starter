681d9580bed317f40f50c0f46333a748bb0e3e59
gomathi5222 <gomathi944566@gmail.com>
2023-08-28 18:28:33 +0530

    SearchView file added

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
+import { async } from 'regenerator-runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 // const recipeContainer = document.querySelector('.recipe');
@@ -36,3 +38,15 @@ const init = function () {
   recipeView.addHandlerRender(controlRecipes);
 }
 init();
+
+const controlSearchResults = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+    await model.loadSearchResults(query)
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+}
+controlSearchResults();
