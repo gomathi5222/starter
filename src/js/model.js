9427d64214d5c6766f3c4419decc60ff3fe79f2c
gomathi5222 <gomathi944566@gmail.com>
2023-08-28 12:56:29 +0530

    helpers.js file added

--- src/js/model.js
+++ src/js/model.js
@@ -1,18 +1,14 @@
 // import { async } from "regenerator-runtime";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 export const state = {
     recipe: {}
 }
 
 export const loadRecipe = async function (id) {
     try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-
-        const data = await res.json();
-
-        console.log(data);
-
-        if (!res.ok) throw new Error(`${data.message}(${res.status})`);
-
+        const data = await getJSON(`${API_URL}/${id}`);
+         
         const { recipe } = data.data;
         state.recipe = {
             id: recipe.id,
@@ -26,7 +22,8 @@ export const loadRecipe = async function (id) {
         }
         console.log(state.recipe);
     } catch (err) {
-        console.log(err);
+        // Temporary error handling
+        console.error(`${err}💥💥💥`);
     }
 
 };
