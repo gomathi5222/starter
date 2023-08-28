df4f3062892f50affdb2840fefaeae67a32b65a5
gomathi5222 <gomathi944566@gmail.com>
2023-08-28 10:37:14 +0530

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,50 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+// const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+// console.log("test");
+// const renderSpinner = function (parentEl) {
+//     const markup = `
+//           <div class="spinner">
+//             <svg>
+//               <use href="${icons}#icon-loader"></use>
+//             </svg>
+//           </div>`;
+//     parentEl.innerHTML = '';
+//     parentEl.insertAdjacentHTML('beforeend', markup);
+// };
+
+const controlRecipes = async function () {
+  try {
+    recipeView.renderSpinner();
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering Recipe
+    recipeView.render(model.state.recipe);
+    console.log(model.state.recipe);
+  } catch (err) {
+    console.error(err);
+  }
+}
+// controlRecipes();
+
+window.addEventListener('hashchange', controlRecipes);
+window.addEventListener('load', controlRecipes);
