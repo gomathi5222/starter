9782fe0358144be5bd17a14a3a7bd5ccc9f4a6fb
gomathi5222 <gomathi944566@gmail.com>
2023-09-11 17:58:12 +0530

    fixed the bug

--- src/js/model.js
+++ src/js/model.js
@@ -48,7 +48,8 @@ export const loadSearchResults = async function (query) {
                 publisher: rec.publisher,
                 image: rec.image_url,
             }
-        })
+        });
+        state.search.page = 1;
     } catch (err) {
         console.error(`${err}💥💥💥`);
         throw err;
